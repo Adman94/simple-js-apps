ff162ca6805b45de52c604784a449b15688770fe
Adman94 <adman00794@gmail.com>
2022-01-29 01:57:49 -0800

    cat generator

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,29 @@
-// alert("hello");
-console.log("sup");
+// Challenge 1: Your Age in Days
+function ageInDays() {
+  var birthYear = prompt("Hello friend, what year were you born?");
+  var ageInDays = (2022 - birthYear) * 365;
+  console.log(ageInDays);
+  var h1 = document.createElement("h1");
+  var textAnswer = document.createTextNode(
+    "You are " + ageInDays + " days old!"
+  );
+  h1.setAttribute("id", "ageInDays");
+  h1.appendChild(textAnswer);
+  document.getElementById("flex-box-result").appendChild(h1);
+}
+
+function reset() {
+  document.getElementById("ageInDays").remove();
+}
+
+function generateCat() {
+  var img = document.createElement("img");
+  var div = document.getElementById("flex-cat-gen");
+  img.src =
+    "https://api.thecatapi.com/api/images/get?format=src&type=gif&size=small";
+  div.appendChild(img);
+}
+
+function resetCatGen() {
+  document.getElementById("flex-cat-gen").innerHTML = "";
+}
